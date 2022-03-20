7d036a78a456bbacf2178378c63516bd1c700d44
Ardans <719481334@qq.com>
2022-03-20 11:25:34 +0800

    fetch:init plum

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,101 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { $ref, $computed } from 'vue/macros';
+import { onMounted } from 'vue';
+
+interface Point {
+  x: number;
+  y: number;
+}
+interface Branch {
+  start: Point;
+  length: number;
+  angel: number;
+}
+const el = $ref<HTMLCanvasElement>();
+const WIDTH = 600;
+const HEIGHT = 600;
+const DEPTH = 3;
+const ctx = $computed(() => el?.getContext('2d'));
+const pendingFrame = [];
+function init() {
+  // lineTo({ x: 30, y: 50 }, { x: 150, y: 100 });
+  const startBranch: Branch = {
+    start: { x: WIDTH / 2, y: HEIGHT },
+    length: 40,
+    angel: -Math.PI / 2,
+  };
+  step(startBranch);
+}
+
+function step(b: Branch, depth: number = 0) {
+  const end = getEndPoint(b);
+  drawBranch(b);
+  if (depth < DEPTH || Math.random() < 0.5) {
+    pendingFrame.push(() =>
+      step(
+        {
+          start: end,
+          length: b.length + (Math.random() * 10 - 5),
+          angel: b.angel - 0.3 * Math.random(),
+        },
+        depth + 1
+      )
+    );
+  }
+  if (depth < DEPTH || Math.random() < 0.5) {
+    pendingFrame.push(() =>
+      step(
+        {
+          start: end,
+          length: b.length + (Math.random() * 10 - 5),
+          angel: b.angel + 0.3 * Math.random(),
+        },
+        depth + 1
+      )
+    );
+  }
+}
+
+function drawFrame() {
+  const frames = [...pendingFrame];
+  pendingFrame.length = 0;
+  frames.forEach((frame) => frame());
+}
+
+function startFrame() {
+  requestAnimationFrame(() => {
+    drawFrame();
+    startFrame();
+  });
+}
+
+startFrame();
+function drawBranch(b: Branch) {
+  const endPoint = getEndPoint(b);
+  lineTo(b.start, endPoint);
+}
+
+function getEndPoint(b: Branch) {
+  return {
+    x: b.start.x + b.length * Math.cos(b.angel),
+    y: b.start.y + b.length * Math.sin(b.angel),
+  };
+}
+
+function lineTo(p1: Point, p2: Point) {
+  ctx.beginPath();
+  ctx.moveTo(p1.x, p1.y);
+  ctx.lineTo(p2.x, p2.y);
+  ctx.stroke();
+}
+
+onMounted(() => {
+  init();
+});
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <canvas ref="el" width="600" height="600" style="border: 1px solid grey"></canvas>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
